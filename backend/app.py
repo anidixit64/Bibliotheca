94dabac8484f0c35597f3d9a9a6934288a450df5
Aniket Dixit <anidixit@umich.edu>
2024-08-21 12:54:11 -0500

    Connected backend to frontend to institute preliminary input screen

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,64 @@
-from flask import Flask
+import os
+from flask import Flask, request, jsonify, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
-@app.route('/')
-def home():
-    return "Welcome to the Bibliotheca!"
+def create_app():
+    app = Flask(__name__, static_folder='../frontend', static_url_path='')
+
+    # Configure the SQLite database to be in the backend directory
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(app.root_path, "books.db")}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    # CRUD routes
+    @app.route('/books', methods=['GET'])
+    def get_books():
+        books = Book.query.all()
+        return jsonify([{'id': book.id, 'title': book.title, 'author': book.author, 'read': book.read} for book in books])
+
+    @app.route('/books', methods=['POST'])
+    def add_book():
+        new_book = Book(title=request.json['title'], author=request.json['author'], read=request.json.get('read', False))
+        db.session.add(new_book)
+        db.session.commit()
+        return jsonify({'id': new_book.id}), 201
+
+    @app.route('/books/<int:book_id>', methods=['PUT'])
+    def update_book(book_id):
+        book = Book.query.get_or_404(book_id)
+        book.title = request.json.get('title', book.title)
+        book.author = request.json.get('author', book.author)
+        book.read = request.json.get('read', book.read)
+        db.session.commit()
+        return jsonify({'id': book.id})
+
+    @app.route('/books/<int:book_id>', methods=['DELETE'])
+    def delete_book(book_id):
+        book = Book.query.get_or_404(book_id)
+        db.session.delete(book)
+        db.session.commit()
+        return '', 204
+
+    # Serve the frontend
+    @app.route('/')
+    def home():
+        return send_from_directory(app.static_folder, 'index.html')
+
+    return app
+
+# Define the Book model
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    author = db.Column(db.String(120), nullable=False)
+    read = db.Column(db.Boolean, default=False)
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
